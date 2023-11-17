97e8869ae93cbbef128dc12abdb2d6ea990bf12d
rifaldiarifin <adirifaldiarifin@gmail.com>
2023-11-17 15:01:55 +0700

    update new stored data (searchpopup)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cihuyCodeReducer from './slices/cihuyCodeSlice'
 import searchPopupReducer from './slices/searchPopupSlice'
+import hotKeysReducer from './slices/hotKeysSlice'
 
 const store = configureStore({
   reducer: {
     // reducers
     cihuyCode: cihuyCodeReducer,
-    searchPopup: searchPopupReducer
+    searchPopup: searchPopupReducer,
+    hotKeys: hotKeysReducer
   }
 })
-
 export default store
