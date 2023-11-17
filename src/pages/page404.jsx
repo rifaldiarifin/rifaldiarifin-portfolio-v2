39ee6e948279823fe2a51e0e5ebbbf14975d6db0
rifaldiarifin <adirifaldiarifin@gmail.com>
2023-11-17 15:03:19 +0700

    create page not found (page 404)

--- src/pages/page404.jsx
+++ src/pages/page404.jsx
@@ -1,5 +1,28 @@
-const Page404 = () => {
-  return <h1>Page404</h1>
+import Button from '../components/Elements/Button'
+import Icons8 from '../components/elements/Icons8'
+import {
+  Bracket,
+  Comment,
+  Control,
+  KeywordOperator,
+  Space,
+  String,
+  Variable,
+  VariableName
+} from '../components/fragments/Codes'
+
+const Page404 = ({ onOpenShortcut }) => {
+  return (
+    <>
+      <div className="page404">
+        <Icons8 icon="cancel" gradient />
+        <p>The editor could not be opened because the file was not found.</p>
+        <Button color="default" style={'fill'} height={'40px'} moreClass={'rounded10'} onClick={onOpenShortcut}>
+          Open Main
+        </Button>
+      </div>
+    </>
+  )
 }
 
 export default Page404
