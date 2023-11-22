f6af79583e2ddbd4af3e117f28b9c0c21526e52e
rifaldiarifin <adirifaldiarifin@gmail.com>
2023-11-22 22:58:23 +0700

    rewrite css, fix bugs

---
+++ src/assets/fonts/Fira_Code_v6.2/fireCode.js
@@ -0,0 +1,14 @@
+import('woff2/FiraCode-Light.woff2')
+import('woff/FiraCode-Light.woff')
+
+import('woff2/FiraCode-Regular.woff2')
+import('woff/FiraCode-Regular.woff')
+
+import('woff2/FiraCode-Medium.woff2')
+import('woff/FiraCode-Medium.woff')
+
+import('woff2/FiraCode-SemiBold.woff2')
+import('woff/FiraCode-SemiBold.woff')
+
+import('woff2/FiraCode-Bold.woff2')
+import('woff/FiraCode-Bold.woff')   
