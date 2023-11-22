aa678de11188edc28fb8c5f3b97f212570f62399
rifaldiarifin <adirifaldiarifin@gmail.com>
2023-11-22 22:51:04 +0700

    change main styling CSS

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
-import PopupAlertContextProvider from './contexts/PopupAlertContext'
 import store from './redux/store'
 import router from './routes'
-import './assets/fonts/Fira_Code_v6.2/fira_code.css'
-import './assets/fonts/JetBrainsMono-2.304/main.css'
-import './assets/fonts/googlefonts/Montserrat/main.css'
-import './assets/fonts/googlefonts/Poppins/main.css'
-import './styles/index.css'
-import 'react-lazy-load-image-component/src/effects/opacity.css'
-import './assets/icons8/fluent/main.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PopupAlertContextProvider>
-        <RouterProvider router={router} />
-      </PopupAlertContextProvider>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 )
