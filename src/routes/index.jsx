ecefef4662f151aa1f8a22477991a3ce0fb81d01
rifaldiarifin <adirifaldiarifin@gmail.com>
2023-11-22 22:53:04 +0700

    Update route, add custom error handling

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,31 @@
+import React, { Suspense } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
-import CihuyCode from '../pages/cihuyCode'
+import LoadingResource from '../components/fragments/LoadingResource'
+import '../assets/fonts/Fira_Code_v6.2/woff2/FiraCode-Regular.woff2'
+import '../assets/fonts/JetBrainsMono-2.304/webfonts/JetBrainsMono-Bold.woff2'
+import '../assets/fonts/JetBrainsMono-2.304/webfonts/JetBrainsMono-ExtraBold.woff2'
+import '../assets/fonts/JetBrainsMono-2.304/webfonts/JetBrainsMono-Regular.woff2'
+import '../assets/fonts/JetBrainsMono-2.304/webfonts/JetBrainsMono-SemiBold.woff2'
+import '../assets/fonts/JetBrainsMono-2.304/webfonts/JetBrainsMono-Light.woff2'
+import '../assets/fonts/googlefonts/Montserrat/static/Montserrat-ExtraBold.ttf'
+import ErrorElement from '../pages/errorElement'
+
+const CihuyCode = React.lazy(() => import('../pages/cihuyCode'))
+const PopupAlertContextProvider = React.lazy(() => import('../contexts/PopupAlertContext'))
+const ToastNotificationProvider = React.lazy(() => import('../contexts/ToastNotificationContext'))
 
 const router = createBrowserRouter([
   // CihuyCode page
   {
     path: '/*',
-    element: <CihuyCode />
+    errorElement: <ErrorElement />,
+    element: <Suspense fallback={<LoadingResource />}>
+      <PopupAlertContextProvider>
+        <ToastNotificationProvider>
+          <CihuyCode />
+        </ToastNotificationProvider>
+      </PopupAlertContextProvider>
+    </Suspense>
   }
 ])
 
