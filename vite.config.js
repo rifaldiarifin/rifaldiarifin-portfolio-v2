962ad409626cf797f499d7fadff84c2fa5dae665
rifaldiarifin <adirifaldiarifin@gmail.com>
2023-11-22 22:46:02 +0700

    Update vite.config.js and add prettierignore CihuyCommand file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+    hmr:{
+      overlay: false,
+    }
   },
   build: {
     rollupOptions: {
