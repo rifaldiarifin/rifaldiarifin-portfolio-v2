4f54911ca8a0254242494ed817fb51d1898ee187
rifaldiarifin <adirifaldiarifin@gmail.com>
2024-01-20 23:05:26 +0700

    disable purgecss plugin

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import pluginPurgeCSS from 'vite-plugin-purge'
+// import pluginPurgeCSS from 'vite-plugin-purge'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    pluginPurgeCSS({
-      content: [
-        'index.html',
-        './src/**/*.js',
-        './src/**/*.jsx',
-        './src/**/**/*.jsx',
-        './src/**/**/**/*.jsx',
-        './src/**/**/**/**/*.jsx'
-      ],
-      css: ['src/index.css']
-    })
+    // pluginPurgeCSS({
+    //   content: [
+    //     'index.html',
+    //     'src/**/*.js',
+    //     'src/**/*.jsx',
+    //     'src/**/**/*.jsx',
+    //     'src/**/**/**/*.jsx',
+    //     'src/**/**/**/**/*.jsx'
+    //   ],
+    //   css: [
+    //     'node_modules/@vscode/codicons/dist/codicon.css',
+    //     'src/index.css', 
+    //     'src/assets/icons8/fluent/main.css',
+    //     'src/assets/fonts/Fira_Code_v6.2/fira_code.css',
+    //     'src/assets/fonts/googlefonts/Montserrat/main.css',
+    //     'src/assets/fonts/JetBrainsMono-2.304/main.css'
+    //   ],
+    //   skippedContentGlobs: ['node_modules/**', 'src/assets/**']
+    // })
   ],
   server: {
     port: 3000,
